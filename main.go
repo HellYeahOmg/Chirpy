f9de5e96c175a1504891547c0afbf2379a419fa3
Vadim <vadim7552@yandex.ru>
2025-08-29 00:09:43 +0200

    feat(api): add metrics

--- main.go
+++ main.go
@@ -1,18 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) resetMetricsInc() {
+	cfg.fileserverHits.Store(0)
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	sm := http.NewServeMux()
+	config := apiConfig{}
+
 	s := http.Server{
 		Handler: sm,
 		Addr:    ":8080",
 	}
 
-	sm.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("./"))))
+	sm.Handle("/app/", config.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir("./")))))
 	sm.Handle("/app/assets", http.StripPrefix("/app/assets", http.FileServer(http.Dir("./assets/"))))
+	sm.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		s := fmt.Sprintf("Hits: %v", config.fileserverHits.Load())
+		w.Write([]byte(s))
+	})
+
+	sm.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+		config.resetMetricsInc()
+		w.Write([]byte("OK"))
+	})
 
 	sm.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
