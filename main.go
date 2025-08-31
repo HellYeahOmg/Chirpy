d6653fe0f437d51887617d90ce014edbcaeef69d
Vadim <vadim7552@yandex.ru>
2025-08-31 16:05:18 +0200

    feat(api): add user create handler

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
-// postgres://vadimdemidov:@localhost:5432/chirpy
-
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/HellYeahOmg/Chirpy/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
@@ -26,6 +30,16 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func main() {
+	godotenv.Load(".env")
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Printf("failed to open db connection: %s", err)
+		panic(1)
+	}
+
+	dbQueries := database.New(db)
+
 	sm := http.NewServeMux()
 	config := apiConfig{}
 
@@ -128,6 +142,50 @@ func main() {
 
 		w.WriteHeader(200)
 		w.Write(dat)
+	})
+
+	sm.HandleFunc("POST /api/users", func(w http.ResponseWriter, r *http.Request) {
+		type parameters struct {
+			Email string `json:"email"`
+		}
+
+		type response struct {
+			ID        string `json:"id"`
+			CreatedAt string `json:"created_at"`
+			UpdatedAt string `json:"updated_at"`
+			Email     string `json:"email"`
+		}
+
+		params := parameters{}
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&params)
+		if err != nil {
+			log.Printf("failed to decoded request body: %s\n", err)
+			w.WriteHeader(500)
+			return
+		}
+
+		dbUser, err := dbQueries.CreateUser(r.Context(), params.Email)
+		if err != nil {
+			log.Printf("failed to create a new user: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+
+		responseBody := User{
+			ID:        dbUser.ID,
+			CreatedAt: dbUser.CreatedAt,
+			UpdatedAt: dbUser.UpdatedAt,
+			Email:     dbUser.Email,
+		}
+
+		data, err := json.Marshal(responseBody)
+		if err != nil {
+			log.Printf("failed to marshal responseBody: %s", err)
+		}
+
+		w.WriteHeader(201)
+		w.Write(data)
 	})
 
 	s.ListenAndServe()
